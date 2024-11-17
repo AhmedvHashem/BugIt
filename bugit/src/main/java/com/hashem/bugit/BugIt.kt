e26c63a8230ebf3157a30ff0a3163a282f1fac5d
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-11-17 18:50:22 +0200

    first draft for bugit lib configuration

--- bugit/src/main/java/com/hashem/bugit/BugIt.kt
+++ bugit/src/main/java/com/hashem/bugit/BugIt.kt
@@ -1,5 +1,39 @@
 package com.hashem.bugit
 
-class BugIt {
+import com.hashem.bugit.data.BugitDataSource
+import com.hashem.bugit.framework.GoogleSheetDataSource
 
+class BugIt private constructor(
+    fields: MutableMap<String, String> = mutableMapOf(),
+    externalConnector: BugitDataSource
+) {
+    class Builder {
+        object CONST {
+            const val KEY_SCREENSHOT = "bug-screenshot"
+            const val KEY_DESCRIPTION = "bug-description"
+        }
+
+
+        private val fields = mutableMapOf<String, String>()
+        private var externalConnector: BugitDataSource? = null
+
+        fun screenShot(screenShot: String) =
+            apply { this.fields[CONST.KEY_SCREENSHOT] = screenShot }
+
+        fun description(description: String) =
+            apply { this.fields[CONST.KEY_DESCRIPTION] = description }
+
+        fun addExtraField(key: String, defaultValue: String) =
+            apply { this.fields[key] = defaultValue }
+
+        fun useExternalConnector(externalConnector: BugitDataSource) = apply {
+            this.externalConnector = externalConnector
+        }
+
+        fun build() = BugIt(fields, externalConnector ?: GoogleSheetDataSource())
+    }
+
+    fun show() {
+
+    }
 }
