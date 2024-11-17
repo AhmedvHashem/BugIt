6e6b7de412c024021362c25a0ae336de80fa97ed
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-11-17 17:46:39 +0200

    adding network permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.hashem.app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hashem.app"
--- app/src/main/java/com/hashem/app/MainActivity.kt
+++ app/src/main/java/com/hashem/app/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.hashem.bugit.app.ui.theme.BugItTheme
+import com.hashem.app.ui.theme.BugItTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
