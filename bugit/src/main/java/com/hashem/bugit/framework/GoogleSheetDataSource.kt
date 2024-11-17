05cbb41fbe9be50fb391bdc8eac9e3b888761124
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-11-17 18:50:00 +0200

    adding google sheet base implemintation files

---
+++ bugit/src/main/java/com/hashem/bugit/framework/GoogleSheetDataSource.kt
@@ -0,0 +1,9 @@
+package com.hashem.bugit.framework
+
+import com.hashem.bugit.data.BugitDataSource
+
+internal class GoogleSheetDataSource : BugitDataSource {
+    override fun report(screenShot: String, fields: Map<String, String>) {
+        TODO("Not yet implemented")
+    }
+}
