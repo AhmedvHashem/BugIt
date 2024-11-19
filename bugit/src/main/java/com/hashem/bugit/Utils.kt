bd6a2abb14b66d1c64740ac247f95c50a1dd4d1a
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-11-19 03:56:50 +0200

    adding utils to convert image to base64 format

---
+++ bugit/src/main/java/com/hashem/bugit/Utils.kt
@@ -0,0 +1,182 @@
+package com.hashem.bugit
+
+import android.content.ContentUris
+import android.content.Context
+import android.database.Cursor
+import android.net.Uri
+import android.os.Environment
+import android.provider.DocumentsContract
+import android.provider.MediaStore
+import android.text.TextUtils
+import android.util.Base64
+import java.io.File
+import java.io.FileInputStream
+import java.io.IOException
+
+object Utils {
+
+    fun getFilePathFromURI(context: Context, uri: Uri): String? {
+        // DocumentProvider
+        if (DocumentsContract.isDocumentUri(context, uri)) {
+            if (isExternalStorageDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+
+                return Environment.getExternalStorageDirectory().toString() + "/" + split[1]
+            } else if (isDownloadsDocument(uri)) {
+                var cursor: Cursor? = null
+                try {
+                    cursor = context.contentResolver.query(
+                        uri,
+                        arrayOf(MediaStore.MediaColumns.DISPLAY_NAME),
+                        null,
+                        null,
+                        null
+                    )
+                    if (cursor != null && cursor.moveToFirst()) {
+                        val fileName = cursor.getString(0)
+                        val path = Environment.getExternalStorageDirectory()
+                            .toString() + "/Download/" + fileName
+                        if (!TextUtils.isEmpty(path)) {
+                            return path
+                        }
+                    }
+                } catch (_: Exception) {
+                } finally {
+                    cursor?.close()
+                }
+                val id = DocumentsContract.getDocumentId(uri)
+                if (!TextUtils.isEmpty(id)) {
+                    if (id.startsWith("raw:")) {
+                        return id.replaceFirst("raw:".toRegex(), "")
+                    }
+                    val contentUriPrefixesToTry = arrayOf(
+                        "content://downloads/public_downloads",
+                        "content://downloads/my_downloads"
+                    )
+                    for (contentUriPrefix in contentUriPrefixesToTry) {
+                        try {
+                            val contentUri = ContentUris.withAppendedId(
+                                Uri.parse(contentUriPrefix),
+                                id.toLong()
+                            )
+
+                            return getDataColumn(context, contentUri, null, null)
+                        } catch (e: NumberFormatException) {
+                            //In Android 8 and Android P the id is not a number
+                            return uri.path!!.replaceFirst("^/document/raw:".toRegex(), "")
+                                .replaceFirst("^raw:".toRegex(), "")
+                        }
+                    }
+                }
+            } else if (isMediaDocument(uri)) {
+                val docId = DocumentsContract.getDocumentId(uri)
+                val split = docId.split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+                val type = split[0]
+
+                var contentUri: Uri? = null
+                if ("image" == type) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                } else if ("video" == type) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI
+                } else if ("audio" == type) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+                }
+
+                val selection = "_id=?"
+                val selectionArgs = arrayOf(split[1])
+
+                return getDataColumn(context, contentUri, selection, selectionArgs)
+            }
+        } else if ("content".equals(uri.scheme, ignoreCase = true)) {
+            if (isGooglePhotosUri(uri)) {
+                return uri.lastPathSegment
+            }
+
+            return getDataColumn(context, uri, null, null)
+        } else if ("file".equals(uri.scheme, ignoreCase = true)) {
+            return uri.path
+        }
+        return null
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private fun isExternalStorageDocument(uri: Uri): Boolean {
+        return "com.android.externalstorage.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private fun isDownloadsDocument(uri: Uri): Boolean {
+        return "com.android.providers.downloads.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private fun isMediaDocument(uri: Uri): Boolean {
+        return "com.android.providers.media.documents" == uri.authority
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is Google Photos.
+     */
+    private fun isGooglePhotosUri(uri: Uri): Boolean {
+        return "com.google.android.apps.photos.content" == uri.authority
+    }
+
+    private fun getDataColumn(
+        context: Context,
+        uri: Uri?,
+        selection: String?,
+        selectionArgs: Array<String>?
+    ): String? {
+        var cursor: Cursor? = null
+        val column = "_data"
+        val projection = arrayOf(column)
+        try {
+            cursor = context.contentResolver.query(
+                uri!!,
+                projection,
+                selection,
+                selectionArgs,
+                null
+            )
+            if (cursor != null && cursor.moveToFirst()) {
+                val columnIndex = cursor.getColumnIndexOrThrow(column)
+                return cursor.getString(columnIndex)
+            }
+        } finally {
+            cursor?.close()
+        }
+        return null
+    }
+
+    fun getFileBase64String(file: String): String? {
+        val byteArray = getFileByteArray(File(file))
+        return Base64.encodeToString(byteArray, Base64.DEFAULT)
+    }
+
+    private fun getFileByteArray(file: File?): ByteArray? {
+        if (file == null) return null
+
+        val byteArray = ByteArray(file.length().toInt())
+
+        try {
+            val stream = FileInputStream(file)
+            stream.read(byteArray)
+            stream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return byteArray
+    }
+}
