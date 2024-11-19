9844ce1ab69fa90a1602135f3067218782c28870
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-11-19 03:56:31 +0200

    renaming

--- bugit/src/main/java/com/hashem/bugit/framework/GoogleSheetDataSource.kt
+++ bugit/src/main/java/com/hashem/bugit/framework/GoogleSheetDataSource.kt
@@ -1,9 +1,17 @@
 package com.hashem.bugit.framework
 
+import android.util.Log
+import com.hashem.bugit.Utils
 import com.hashem.bugit.data.BugitDataSource
 
+//https://script.google.com/macros/s/AKfycbxA1__FJ0WTGweQXxhjKS1CCjveVpT2TXdiiCUuQabUkGqAtp3HM-cNZdPXzcxCRixi/exec
 internal class GoogleSheetDataSource : BugitDataSource {
-    override fun report(screenShot: String, fields: Map<String, String>) {
-        TODO("Not yet implemented")
+    override suspend fun report(image: String, fields: Map<String, String>) {
+        Log.e("GoogleSheetDataSource", "screenShot: $image, fields: $fields")
+        Log.e("GoogleSheetDataSource", "Thread name: ${Thread.currentThread().name}")
+
+        val imageBase64 = Utils.getFileBase64String(image)
+        Log.e("GoogleSheetDataSource", "imageBase64: $imageBase64")
+
     }
 }
