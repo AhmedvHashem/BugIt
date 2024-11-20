b37d95d1a9846fe6f4a7fd1e6eaadc68cae83aa7
Ahmed Hashem <ahmedvhashem@gmail.com>
2024-11-20 02:38:26 +0200

    deleted BugResult.kt and depends on throwing exception for error handling on domain and data layers

--- bugit/src/main/java/com/hashem/bugit/domain/BugResult.kt
+++
@@ -1,6 +0,0 @@
-package com.hashem.bugit.domain
-
-sealed class BugResult<out R> {
-    data class Success<out T>(val data: T) : BugResult<T>()
-    data class Error(val exception: Exception) : BugResult<Nothing>()
-}
