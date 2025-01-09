04e88a25f3540157b2db857f84fa5692d67dd1f5
Ahmed Hashem <ahmedvhashem@gmail.com>
2025-01-10 01:09:39 +0200

    fixing api 35 issue with external storage permission

--- bugit/src/main/java/com/hashem/bugit/ui/BugItViewModel.kt
+++ bugit/src/main/java/com/hashem/bugit/ui/BugItViewModel.kt
@@ -8,8 +8,6 @@ import androidx.lifecycle.viewmodel.viewModelFactory
 import com.hashem.bugit.BugIt
 import com.hashem.bugit.data.DefaultBugRepository
 import com.hashem.bugit.domain.ReportBugUseCase
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
